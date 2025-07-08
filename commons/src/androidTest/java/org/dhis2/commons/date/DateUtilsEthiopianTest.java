8ffc536020bd0a5777ca14290e694b4bdd6362bd
Bereket Alemayehu <134443629+bereket-alemayehu@users.noreply.github.com>
2025-07-08 06:18:24 +0000

    ✅ Update Ethiopian calendar test file to trigger workflow

--- commons/src/androidTest/java/org/dhis2/commons/date/DateUtilsEthiopianTest.java
+++ commons/src/androidTest/java/org/dhis2/commons/date/DateUtilsEthiopianTest.java
@@ -19,5 +19,10 @@ public class DateUtilsEthiopianTest {
         assertEquals("2017-07-07", eth);
     }
 
-
+    @Test
+    public void testEthiopianToGregorian() {
+        Date greg = DateUtils.convertEthiopianToGregorian(2017, 7, 7);
+        LocalDate localDate = greg.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        assertEquals(LocalDate.of(2025, 7, 7), localDate);
+    }
 }
